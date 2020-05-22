4b3258c3866b733b3355bb64501e37cc3fd23f28
Sibel <sbaslamisli@yahoo.com>
2020-05-21 22:29:07 -0400

    adding item to cart

--- app.js
+++ app.js
@@ -69,25 +69,38 @@ class UI {
             }
             button.addEventListener('click', (event) => {
                 event.target.innerText = "In Cart";
-                event.target.disabled = ture;
+                event.target.disabled = true;
                 //get product from products 
-                let cartItem = Storage.getProduct(id);
-
+                let cartItem = {...Storage.getProduct(id), amount: 1 };
+                console.log(cartItem);
                 //add product to the cart
-
+                cart = [...cart, cartItem];
                 //save cart in local storage
-
+                Storage.saveCart(cart);
                 //set cart values 
-
+                this.setCartValues(cart);
                 //add cart item
 
                 //display cart item
 
                 //show the cart
 
-            })
+            });
 
+        });
+    }
+
+
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount
         })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+        cartItems.innerText = itemsTotal;
+        console.log(cartTotal, cartItems)
     }
 }
 
@@ -97,8 +110,11 @@ class Storage {
         localStorage.setItem("products", JSON.stringify(products));
     }
     static getProduct(id) {
-        let products = JSON.parse(localStorage.getItem('product'));
+        let products = JSON.parse(localStorage.getItem('products'));
         return products.find(product => product.id === id);
+    }
+    static saveCart(cart) {
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 }
 
